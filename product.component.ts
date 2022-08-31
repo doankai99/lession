55e20af8b894ea2acaa1691024798643f727443a
Nguyễn Xuân Đoàn <88135534+doankai99@users.noreply.github.com>
2022-08-31 12:02:37 +0700

    Add files via upload

---
+++ product.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  searchText : any;
+    products = [
+      {
+        id : 1,
+        name : 'Iphone Xs',
+        img : 'https://product.hstatic.net/1000080424/product/636767481293463872_iphone-xs-gold-4_b5f892da204e4205ae73434b3db1f80d.png',
+      },
+
+      {
+        id : 2,
+        name : 'Samsung',
+        img : 'https://cdn.tgdd.vn/Products/Images/42/217935/samsung-galaxy-s20-xam-600x600-200x200.jpg',
+      },
+
+      {
+        id : 3,
+        name : 'Oppo',
+        img : 'https://rukminim1.flixcart.com/image/416/416/jrp8r680/cases-covers/back-cover/h/q/d/noble-nz-opo-f7-tran-01-original-imaf6ghkemmmmnmh.jpeg?q=70',
+      },
+    ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
